79e2022a75082e8982efab0ea93505ae45ac5a82
Chetan Ade <chetan.ade98@gmail.com>
2020-03-23 14:08:23 +0530

    avoided overwrite warning

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from pyScripts.demos import simulate_plume_model
 import datetime
+import os
+from os import path
 
 app = Flask(__name__, static_folder='static')
+anim_path = 'static/simulation.mp4'
 
 
 @app.route("/")
@@ -26,10 +29,11 @@ def disp_output():
 
     fig, ax, anim = simulate_plume_model(
         latLng=latLng, start_datetimeObject=datetime.datetime(year, month, day))
-    anim_path = 'static/simulation.mp4'
 
     return render_template('index.html', output_path=anim_path)
 
 
 if __name__ == "__main__":
+    if path.exists(anim_path):
+        os.remove(anim_path)
     app.run()
