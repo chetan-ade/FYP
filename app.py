d1a9f4ecaa1e8f5a338ef79c5b95f6e0eada1315
Chetan Ade <chetan.ade98@gmail.com>
2020-04-18 23:02:12 +0530

    Cleaned app.py, index.html, index.css

--- app.py
+++ app.py
@@ -1,49 +1,56 @@
 from flask import Flask, render_template, request
 from pyScripts.demos import simulate_plume_model
+from os import path
 import datetime
 import os
-from os import path
 
+# PyFlask instance to render HTML pages and simulation
 app = Flask(__name__, static_folder='static')
+
+# Path to the final output video
 anim_path = 'static\simulation.mp4'
+
+# Map used as simulation background
 map_path = 'static\map.png'
 
-
+# Rendering index.html on startup
 @app.route("/")
 def index():
     return render_template('index.html', visibility="hidden")
 
-
+# Function to accept Form Input
 @app.route("/", methods=['POST'])
 def disp_output():
     loc = request.form['inputLoc']
+
     loc_lat = request.form['locLat']
     loc_lng = request.form['locLng']
+
     date = request.form['dateInput']
-    latLng = loc_lat+','+loc_lng
-    day = int(date.split('/')[1])
-    month = int(date.split('/')[0])
-    year = int(date.split('/')[2])
-    print("Location: ", loc)
-    print("Coordinates: ", latLng)
+
+    # The coordinates of input location
+    latLng = loc_lat + ',' + loc_lng
+
+    # Date entered by user
+    month, day, year = [int(i) for i in date.split('/')]
+
+    print("\nLocation:", loc, "\nCoordinates:", latLng)
     print("Day:", day, "\nMonth:", month, "\nYear:", year)
 
-    # latLng = '21.238611,73.350000'  # Best Case
-    # latLng = '19.033000,73.029700'  # Worst Case
-    # latLng = '37.4245,141.0298'  # Fukushima
-    # day = 12
-    # month = 3
-    # year = 2011
+    # Removing existing output, if present.
+    if path.exists(anim_path):
+        os.remove(anim_path)
 
-    fig, ax, anim = simulate_plume_model(
-        latLng=latLng, start_datetimeObject=datetime.datetime(year, month, day))
+    if path.exists(map_path):
+        os.remove(map_path)
 
+    # Function to start simulation
+    # fig, ax, anim = simulate_plume_model(
+    #     latLng=latLng, start_datetimeObject=datetime.datetime(year, month, day))
+
+    # Rendering the output video
     return render_template('index.html', output_path=anim_path, visibility="visible")
 
 
 if __name__ == "__main__":
-    if path.exists(anim_path):
-        os.remove(anim_path)
-    if path.exists(map_path):
-        os.remove(map_path)
     app.run()
